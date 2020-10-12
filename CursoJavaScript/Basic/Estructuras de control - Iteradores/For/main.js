77e691b696dda4494c8aa4bbe0e871e16dae6cab
Alejandro Rivera <riveraalejandro1999@gmail.com>
2020-10-12 09:54:26 -0600

    Basic

---
+++ CursoJavaScript/Basic/Estructuras de control - Iteradores/For/main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+//  Bucle For
+//  Bucle es una estructura de control que se repite varias veces.
+
+var numeros = 100;
+
+for(var i = 0; i <= 100; i++){
+    console.log("Vamos por el numero: " + i);
+}
---
+++ CursoJavaScript/Basic/Estructuras de control - Iteradores/While/main.js
@@ -0,0 +1,21 @@
+'use strict'
+
+//Bucle while 
+
+var year = 2018;
+
+while(year <= 2051){
+    // Ejecuta esto
+    console.log("Estamos en el año: " + year);
+
+    year ++;
+}
+
+// Do while
+
+var years=30;
+
+do{
+    alert("Solo cuando sea diferente a 20");
+    years--;
+}while(years >25);
